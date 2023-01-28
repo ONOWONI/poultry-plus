5208198ed5c6e541c304abbfd92463e4e318679f
Onowoni <danielonowoni1@gmail.com>
2023-01-28 12:54:13 +0100

    deployment7

--- poultryplus/settings.py
+++ poultryplus/settings.py
@@ -17,6 +17,10 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-6753.up.railway.app/'
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -53,7 +57,7 @@ ROOT_URLCONF = "poultryplus.urls"
 
 ACCOUNT_SIGNUP_FORM_CLASS = 'base_app.forms.SignupForm'
 AUTH_USER_MODEL = "base_app.CustomUser"
-ALLOWED_HOSTS = ["localhost"]
+
 
 TEMPLATES = [
     {
@@ -132,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static/"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
